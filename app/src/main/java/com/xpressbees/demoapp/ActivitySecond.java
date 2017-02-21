6726ee0a214bb4f1e6b428acbb785e07596331dd
surajgirigosavi <suraj.m.girigosavi@gmail.com>
2017-02-21 12:58:48 +0530

    Added New Activity (Second) and layout file

---
+++ app/src/main/java/com/xpressbees/demoapp/ActivitySecond.java
@@ -0,0 +1,20 @@
+package com.xpressbees.demoapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by surajgirigosavi on 21/2/17.
+ */
+
+public class ActivitySecond extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+         setContentView(R.layout.activity_second);
+
+
+    }
+}
--- app/src/main/java/com/xpressbees/demoapp/MainActivity.java
+++ app/src/main/java/com/xpressbees/demoapp/MainActivity.java
@@ -1,10 +1,12 @@
 package com.xpressbees.demoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +15,34 @@ public class MainActivity extends AppCompatActivity {
         Log.d("test","on create");
 
         Log.d("test","comment 2");
+        intiViews();
+
+    }
+
+    private void intiViews() {
+
+         findViewById(R.id.btnMainOk).setOnClickListener(this);
+
+    }
+
+    public void click(){
+
+        Log.d("test","click");
+        Intent intent = new Intent(MainActivity.this,ActivitySecond.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.btnMainOk:
+                 click();
+                 break;
+
+        }
 
     }
 }
