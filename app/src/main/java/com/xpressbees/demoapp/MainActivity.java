37fe17938a2edc69c2179eeea4f33cc17be14eeb
surajgirigosavi <suraj.m.girigosavi@gmail.com>
2017-02-21 12:41:06 +0530

    Demo app added

--- app/src/main/java/com/xpressbees/demoapp/MainActivity.java
+++ app/src/main/java/com/xpressbees/demoapp/MainActivity.java
@@ -12,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.d("test","on create");
 
+        Log.d("test","comment 2");
+
     }
 }
